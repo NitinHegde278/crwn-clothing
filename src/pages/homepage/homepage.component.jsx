7f6abd63839ab900705bbdfa1dd56e3314ab5de3
NitinHegde278 <nhnitinhegde24@gmail.com>
2022-03-15 22:26:53 +0530

    Divided into components with scss

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import "./homepage.styles.scss";
+import DirectoryMenu from "../../components/directory-menu/directory-menu.component";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  );
+};
+
+export default HomePage;
