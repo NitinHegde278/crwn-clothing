bc8c5d2d3bdc5193099857c3b7074b2421d4d102
NitinHegde278 <nhnitinhegde24@gmail.com>
2022-03-24 16:45:39 +0530

    Added checkout page with functionality to remove and change quantity with total

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -6,6 +7,17 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    if (cartItems.length) {
+      navigate("/checkout");
+    } else {
+      alert("Cart is empty!");
+    }
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -15,7 +27,7 @@ const CartDropdown = () => {
             })
           : "Your cart is empty! Start adding items"}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import "./checkout-item.styles.scss";
+
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  const { addItemToCart, removeItemFromCart, removeItemFromCheckout } =
+    useContext(CartContext);
+
+  const incrementHandler = (cartItem) => {
+    addItemToCart(cartItem);
+  };
+  const decrementHandler = (cartItem) => {
+    removeItemFromCart(cartItem);
+  };
+
+  const removeHandler = (cartItem) => {
+    removeItemFromCheckout(cartItem);
+  };
+
+  return (
+    <div className="checkout-item-container">
+      <div className="image-container">
+        <img src={imageUrl} alt={name} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => decrementHandler(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => incrementHandler(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{`${quantity} x ${price} = ${
+        quantity * price
+      }`}</span>
+      <div className="remove-button" onClick={() => removeHandler(cartItem)}>
+        &#10006;
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutItem;
